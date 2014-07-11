f7558adba247d66bf221af09b5c9c8810151c848
Eric Mittelhammer <code@ericmittelhammer.com>
2014-07-11 07:52:36 -0400

    adding event handling and output display

--- jsx/nnviz.jsx
+++ jsx/nnviz.jsx
@@ -10,10 +10,15 @@ function lookupsInOrder(lookup) {
 
 
 var Node = React.createClass({
+
+  updated: function() {
+    this.props.updateNetwork(this.refs.outputValue.getDOMNode().innerHtml);
+  },
+
   render: function() {
     return(<g>
       <circle cx={this.props.cx} cy={this.props.cy} r={this.props.radius} fill="#3498db" />
-      <text x={this.props.cx} y={this.props.cy}>{this.props.label}</text>
+      <text x={this.props.cx - this.props.radius} y={this.props.cy - (this.props.radius *0.25)}>{this.props.output}</text>
       </g>
     );
   }
@@ -22,11 +27,10 @@ var Node = React.createClass({
 var NetworkLayer = React.createClass({
   render: function() {
     var g = this.props.geometry;
-    console.log(g);
+    console.log(this.props.outputs);
     var nodes = g.nodes.map(function(node, index){
       var label = '';
-      if 
-      return(<Node cx={node.cx} cy={node.cy} radius={g.radius} label={this.props.values[index]}/>)
+      return(<Node cx={node.cx} cy={node.cy} radius={g.radius} output={this.props.outputs[index] } />)
     }, this);
       
     return(<g>{nodes}</g>);
@@ -57,17 +61,34 @@ var ConnectionSet = React.createClass({
 });
 
 var Network = React.createClass({
+
+    getInitialState: function() {
+      return { network: this.props.initialNetwork }
+    },
+
+    updateNetwork: function(index, value) {
+      var inputs = this.state.network.outputs[0] //get the values of the first layer of neurons
+      inputs[index] = value;
+      this.state.network.run({a:0, b:1});
+    },
+
     render: function() {
       //var x = 1000;
       //var y = x * (this.props.height / this.props.width);
-      var sizes = this.props.network.sizes;
+      var sizes = this.state.network.sizes;
       var canvasWidth = this.props.canvasWidth;
       var canvasHeight = this.props.canvasHeight;
       var g = this.props.geometry;
 
       var networkLayers = g.layers.map(function(l, index){
+        var labels = [];
+        if (index == 0) {
+          labels = lookupsInOrder(this.state.network.inputLookup);
+        }else if (index == g.layers.length-1) {
+          labels = lookupsInOrder(this.state.network.outputLookup);
+        }
         return(
-          <NetworkLayer geometry={l} values={this.props.network.layers[index]}/>
+          <NetworkLayer geometry={l} outputs={this.state.network.outputs[index]} updateNetwork={(index == 0) ? this.updateNetwork : function(a,b){} }/>
         );
       }, this);
 
@@ -77,6 +98,8 @@ var Network = React.createClass({
           <ConnectionSet geometry={connectionSet} />
         );
       }, this);
+
+      console.log(this);
 
       return (
         <svg width={this.props.width} height={this.props.height}>
@@ -96,7 +119,7 @@ var NNViz = (function(nnviz, React, brain, doc, _){
       var container = doc.getElementById(elementId);
       console.log(nnviz);
       var geometry = nnviz.geometry.getGeometry(net.sizes, container.clientWidth, container.clientHeight);
-      React.renderComponent(<Network network={net} canvasWidth={container.clientWidth} canvasHeight={container.clientHeight} geometry={geometry}/>, container);
+      React.renderComponent(<Network initialNetwork={net} canvasWidth={container.clientWidth} canvasHeight={container.clientHeight} geometry={geometry}/>, container);
     }
 
     return nnviz;
